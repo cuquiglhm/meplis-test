f1583dd794e87d29b398d65cf4bf93e79a5a96cc
Guilherme Cuqui <guilherme.cuqui@darwinseguros.com>
2022-07-08 00:18:31 -0300

    bootstrap added / home page / angular cfg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const INITIAL_COMPONENT = HomeComponent;
+
+const ROUTES: Routes = [
+  { path: '', pathMatch: 'full', component: INITIAL_COMPONENT }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
